1f796c1357ebd77189474a7a53a51de6ce0c6d93
konradrog <konrad1210@gmail.com>
2017-09-01 15:45:59 +0200

    new reservation, show reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,5 +4,27 @@ class ReservationsController < ApplicationController
   end
 
   def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.create(reservation_params)
+
+    if @reservation.new_record?
+      render "new"
+    else
+      redirect_to reservations_path(@reservation)
+    end
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
+    @car = Car.find(@reservation.car_id)
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:name, :surname, :e_mail, :from_date, :to_date, :car_id)
   end
 end
