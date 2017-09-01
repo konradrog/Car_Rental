013ef177a88932f3e3c494d11ac734a4fe5ce007
konradrog <konrad1210@gmail.com>
2017-09-01 14:53:01 +0200

    Add empty controller Reservation. Working Adding new Car, show car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -9,5 +9,25 @@ class CarsController < ApplicationController
     @car = Car.new
   end
 
+  def create
+    @car = Car.create(car_params)
+
+    if @car.new_record?
+      render "new"
+    else
+      redirect_to car_path(@car)
+    end
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model,
+                                :year, :price, :description)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :admins
 
   root to: "cars#index"
-  resources :cars
+  resources :cars do
+    resources :opinions
+  end
+  resources :reservations
+
+
   #devise_for :admins# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
