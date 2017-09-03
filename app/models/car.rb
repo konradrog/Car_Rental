b03b19ec4fd09f7c766a71a390aef3e015505309
konradrog <konrad1210@gmail.com>
2017-09-03 16:01:17 +0200

    add validations to car modell. Add error view partial

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,4 +3,11 @@ class Car < ApplicationRecord
   has_many :opinions
 
   mount_uploader :image, ImageUploader
+
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :year, presence: true
+  validates :price, presence: true, numericality: {greather_than: 0 }
+  validates :description, presence: true
+
 end
