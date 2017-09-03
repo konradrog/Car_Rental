e403a708745473c20d516206ceeddf6a18d35047
konradrog <konrad1210@gmail.com>
2017-09-03 18:09:07 +0200

    add validation/present to reservation. Add form partial and error view to reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,3 +1,12 @@
 class Reservation < ApplicationRecord
   belongs_to :car
+
+  validates :name, presence: true
+  validates :surname, presence: true
+  validates :e_mail, presence: true
+  validates :address, presence: true
+  validates :from_date, presence: true
+  validates :to_date, presence: true
+  validates :car_id, presence: true
+
 end
